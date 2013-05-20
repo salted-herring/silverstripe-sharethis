e9e9d7e0491a77b80b7b9b9b44dd3791d4b985ed
nicolaas <nicolaas@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2013-05-20 08:51:25 +0000

    Facebook enhancements
    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/sharethis/branches/2.4@4698 7b40dde9-fc51-43d4-b609-63a66f0a0cee

--- code/api/FacebookFeed.php
+++ code/api/FacebookFeed.php
@@ -51,10 +51,11 @@ class FacebookFeed_Page extends DataObject {
 					<li>Open a new tab and open <a href=\"http://www.facebook.com\" target=\"_blank\">facebook</a></li>
 					<li>Find your page (e.g. https://www.facebook.com/EOSAsia)</li>
 					<li>Note the name (e.g. EOSAsia)</li>
-					<li>Go to <a href=\"http://findmyfacebookid.com\">http://findmyfacebookid.com</a></li>
+					<li>Go to <a href=\"http://findmyfacebookid.com\" target=\"_blank\">http://findmyfacebookid.com</a></li>
 					<li>Enter http://www.facebook.com/EOSAsia</li>
 					<li>You'll get the answer (e.g. 357864420974239)</li>
-					<li>Add this ID to the link - like so: https://www.facebook.com/feeds/page.php?format=rss20&id=357864420974239</li>
+					<li>Add this ID to the link - like this: https://www.facebook.com/feeds/page.php?format=rss20&id=357864420974239</li>
+					<li>This is the link you need to add in the field above</li>
 				</ol>"
 			)
 		);
